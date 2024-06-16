88bdb0cff1314e66fbb7e0b6a2b856b6d80145f6
antipole2 <github@antipole.co.uk>
2024-06-16 08:34:37 +0200

    tweek with new Date

--- vdr2.js
+++ vdr2.js
@@ -444,10 +444,10 @@ function tidyUp(){
 
 function checkForUpdates(){
 	if (!OCPNisOnline()) return;
+	now = new Date().getTime();
 	checkDays = 5;	// how often to check
 	if (_remember.hasOwnProperty("versionControl")){
 		if (trace) print("_remember: ", JSON.stringify(_remember), "\n");
-		now = new Date().getTime();
 		lastCheck = _remember.versionControl.lastCheck;
 		nextCheck = lastCheck + checkDays*24*60*60*1000;
 		if (trace) print("now: ", now, "\tversionControl.lastCheck was ", lastCheck, "\tnext due ", nextCheck, "\n");
